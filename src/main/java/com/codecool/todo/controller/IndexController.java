257c5a5200bc940e98c2dd215c0b5ef8799a8c79
Vidó Márk <vidomark42@gmail.com>
2021-06-22 15:24:10 +0200

    Project upload - basic spring application uploaded

---
+++ src/main/java/com/codecool/todo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.codecool.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/codecool/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
